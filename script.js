df79f0f9a1a76ae392085412465d7503b34844af
Gary Do <garydo050915@gmail.com>
2026-02-20 13:19:14 +1100

    Added script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("JavaScript is now working")
+
+const button = document.querySelector(".btn")
+
+button.addEventListener("click", function() {
+    alert("Thanks for visiting my portfolio!")
+})
