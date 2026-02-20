5fb27dac0874d22195cf1dc0d529e8b57ab69f3b
Gary Do <garydo050915@gmail.com>
2026-02-20 16:54:37 +1100

    Added dark and light mode

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ console.log("JavaScript is now working")
 
 const button = document.querySelector(".btn")
 
+const themeBtn = document.querySelector(".themebtn")
+
+themeBtn.addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode")
+
+    if (document.body.classList.contains("dark-mode")){
+        themeBtn.textContent = "Light Mode"
+    } else {
+        themeBtn.textContent = "Dark Mode"
+    }
+})
+
 button.addEventListener("click", function() {
     alert("Thanks for visiting my portfolio!")
 })
